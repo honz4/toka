c74cf505eed912ce15da158cf56df4f72f573ffb
charles.childers <charles.childers@ba2d145d-6729-0410-afd8-f948aadcc4f3>
2007-03-24 01:44:19 +0000

    Added { } and variable| to the bootstrap.toka. Put some comments into needdl.c

--- other/needdl.c
+++ other/needdl.c
@@ -1,1 +1,6 @@
+/* This is a dummy file used to help us detect */
+/* the need for libdl.so                       */
+/* Look at the LDFLAGS in the Makefile to see  */
+/* how it works.                               */
+
 void main() {}
