f6779c1619c685af5a35d4342a9fc27b1cfcb3ad
charles.childers <charles.childers@ba2d145d-6729-0410-afd8-f948aadcc4f3>
2007-04-21 20:55:01 +0000

    Stack errors are now handled by the error() routine. This provides more detailed reporting, and should allow for recovery of data stack over/underflow in most cases. Return stack problems are fatal.

--- trunk/source/errors.c
+++ trunk/source/errors.c
@@ -27,16 +27,20 @@ extern long stack[], rstack[], sp, rsp;
  ******************************************************/
 void error(long code)
 {
+  long fatal;
+
+  fatal = FALSE;
+
   printf("E%li: ", code);
   switch (code)
   {
     case ERROR_WORD_NOT_FOUND:
-         printf("%s is not a word or a number.\n", (char *)TOS);
+         printf("'%s' is not a word or a number.\n", (char *)TOS);
          DROP;
          break;
     case ERROR_NO_MEM:
-         printf("Out of memory; aborting.\n");
-         exit(1);
+         printf("Out of memory\n");
+         fatal = TRUE;
          break;
     case ERROR_FFI_ARGUMENTS:
          printf("Too many arguments to alien function.\n");
@@ -49,5 +53,35 @@ void error(long code)
          printf("Lookup of symbol '%s' failed.\n", (char *)TOS);
          DROP;
          break;
+    case ERROR_STACK:
+         printf("Stack problem: ");
+         if (sp < 0)
+         {
+           printf("data stack underflow\n");
+           sp = 0;
+           TOS = 0;
+         }
+         if (sp > 127)
+         {
+           printf("data stack overflow\n");
+           sp = 0;
+           TOS = 0;
+         }
+         if (rsp < 0)
+         {
+           printf("return stack underflow\n");
+           fatal = TRUE;
+         }
+         if (rsp > 1023)
+         {
+           printf("return stack overflow\n");
+           fatal = TRUE;
+         }
+         break;
+  }
+  if (fatal == TRUE)
+  {
+    printf("Unable to recover; will abort...\n\n");
+    exit(1);
   }
 }
--- trunk/source/headers/errors.h
+++ trunk/source/headers/errors.h
@@ -4,3 +4,4 @@ void error(long code);
 #define ERROR_FFI_ARGUMENTS            2
 #define ERROR_FFI_LIBRARY_NOT_FOUND    3
 #define ERROR_FFI_FUNCTION_NOT_FOUND   4
+#define ERROR_STACK                    5
