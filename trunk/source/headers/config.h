ca8d598fde3d50ac96586e9fcbca813a2b7823b9
charles.childers <charles.childers@ba2d145d-6729-0410-afd8-f948aadcc4f3>
2007-06-12 23:44:20 +0000

    Started some work on supporting a native Win32 build in the main source tree (paths for bootstrap.toka and library now set to c:\toka\). Also some changes to the Makefile

--- trunk/source/headers/config.h
+++ trunk/source/headers/config.h
@@ -1,2 +1,9 @@
+#ifdef UNIX
 #define LIBRARY "/usr/share/toka/library/"
 #define BOOTSTRAP "/usr/share/toka/bootstrap.toka"
+#endif
+
+#ifdef WIN32
+#define LIBRARY "c:\\toka\\library\\"
+#define BOOTSTRAP "c:\\toka\\bootstrap.toka"
+#endif
