77102a55c7f38bf637562ed65039b44f94e4f695
charles.childers <charles.childers@ba2d145d-6729-0410-afd8-f948aadcc4f3>
2007-07-11 01:15:40 +0000

    Began work on 'aline' (draw a line by angle).

--- library/C/sdl.c
+++ library/C/sdl.c
@@ -7,6 +7,7 @@
  ***************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <SDL/SDL.h>
 
 
@@ -108,10 +109,11 @@ void sdl_vline(long x, long y1, long y2, long color)
   sdl_render();
 }
 
+
 void sdl_line(long x1, long y1, long x2, long y2, long color)
 {
   long dx, dy, inx, iny, e;
-        
+
   dx = x2 - x1;
   dy = y2 - y1;
   inx = dx > 0 ? 1 : -1;
@@ -120,32 +122,39 @@ void sdl_line(long x1, long y1, long x2, long y2, long color)
   dx = abs(dx);
   dy = abs(dy);
        
-  if(dx >= dy) {
+  if (dx >= dy)
+  {
     dy <<= 1;
     e = dy - dx;
     dx <<= 1;
-      while (x1 != x2) {
-        sdl_putpixel(x1, y1, color);
-        if(e >= 0) {
-          y1 += iny;
-          e-= dx;
-        }
-        e += dy; x1 += inx;
+    while (x1 != x2)
+    {
+      sdl_putpixel(x1, y1, color);
+      if (e >= 0)
+      {
+        y1 += iny;
+        e-= dx;
       }
-  } else {
-   dx <<= 1;
-                e = dx - dy;
-                dy <<= 1;
-                while (y1 != y2) {
-                        sdl_putpixel(x1, y1, color);
-                        if(e >= 0) {
-                                x1 += inx;
-                                e -= dy;
-                        }
-                        e += dx; y1 += iny;
-                }
-        }
-        sdl_putpixel(x1, y1, color);
+      e += dy; x1 += inx;
+    }
+  } 
+  else
+  {
+    dx <<= 1;
+    e = dx - dy;
+    dy <<= 1;
+    while (y1 != y2)
+    {
+      sdl_putpixel(x1, y1, color);
+      if (e >= 0)
+      {
+        x1 += inx;
+        e -= dy;
+      }
+      e += dx; y1 += iny;
+    }
+  }
+  sdl_putpixel(x1, y1, color);
 }
 
 void sdl_box(long x1, long y1, long x2, long y2, long color)
@@ -170,4 +179,34 @@ long sdl_event_mousecoord_y(SDL_Event *event)
 long sdl_event_keysym(SDL_Event *event)
 {
   return (long)event->key.keysym.sym;
+}
+
+
+void sdl_angle_line(long x, long y, long angle, long dist, long color)
+{
+  double x2, y2;
+  long xa, ya, signa, signb;
+
+  x2 = dist * cos(angle);
+  y2 = dist * sin(angle);
+
+  signa = 1;
+  signb = 1;
+
+  if (x2 < 0)
+    signa = -1;
+  if (y2 < 0)
+    signb = -1;
+
+  x2 = abs(x2);
+  y2 = abs(y2);
+
+  xa = (long)x2;
+  ya = (long)y2;
+
+  xa = xa * signa;
+  ya = ya * signb;
+
+  sdl_line(x, y, xa + x, ya + y, color);
+  sdl_render();
 }
